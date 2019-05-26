64054442f5e833ef638430511511c3e32bea630c
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2019-05-26 20:32:19 -0300

    Testing version, average during the interval, compensation for measurment duration

--- sensor.py
+++ sensor.py
@@ -12,11 +12,31 @@ c.execute('''CREATE TABLE IF NOT EXISTS Ambient
 	Humidity REAL NOT NULL,
 	Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
 conn.commit()
+DATA_TIME = 300 # In seconds
+MEASUREMENTS_PER_INTERVAL = 50 
+MEASUREMENT_TIME = DATA_TIME // MEASUREMENTS_PER_INTERVAL
+
+measurements = []
+start = time.time()    
 while True:
-	hum, temp = Adafruit_DHT.read_retry(11,4)
-	print('Temp: {0:0.1f} C  Hum: {1:0.1f} %'.format(temp, hum))
-	c = conn.cursor()
-	c.execute('''INSERT INTO Ambient (Humidity,Temperature) VALUES
-		({0:0.1f},{1:0.1f})'''.format(hum,temp))
-	conn.commit()
-	time.sleep(300)
+	start_measure = time.time()
+    t = Adafruit_DHT.read_retry(11,4)
+    elapsed_measure = time.time() - start_measure
+    measurements.append(t)
+    print('Temp: {0:0.3f} C  Hum: {1:0.3f} %'.format(t[1], t[0]))
+    if len(measurements) == MEASUREMENTS_PER_INTERVAL:
+        avg_hum = sum([t[0] for t in measurements])/MEASUREMENTS_PER_INTERVAL
+        avg_temp = sum([t[1] for t in measurements])/MEASUREMENTS_PER_INTERVAL
+        print('Temp: {0:0.3f} C  Hum: {1:0.3f} %'.format(avg_temp, avg_hum))
+        # Insert into DB
+		c = conn.cursor()
+		c.execute('''INSERT INTO Ambient (Humidity,Temperature) VALUES
+		({0:0.3f},{1:0.3f})'''.format(avg_hum,avg_temp))
+		conn.commit()
+        # Reset queue
+        elapsed = time.time() - start
+        print("Total elapsed time: " + str(elapsed))
+        start = time.time()
+        measurements = []
+    if MEASUREMENT_TIME - elapsed_measure > 0:
+        time.sleep(MEASUREMENT_TIME - elapsed_measure)
--- sensor_test.py
+++ sensor_test.py
@@ -3,21 +3,27 @@ import Adafruit_DHT
 import sqlite3
 import time
 
-DATA_TIME = 10 # In seconds
-MEASURMENTS_PER_INTERVAL = 10 
-MEASURMENT_TIME = DATA_INTERVAL // MEASURMENTS_PER_INTERVAL
+DATA_TIME = 300 # In seconds
+MEASUREMENTS_PER_INTERVAL = 50 
+MEASUREMENT_TIME = DATA_TIME // MEASUREMENTS_PER_INTERVAL
 
-measurments = []    
+measurements = []
+start = time.time()    
 while True:
-	t = Adafruit_DHT.read_retry(11,4)
-    maesurments.append(t)
+    start_measure = time.time()
+    t = Adafruit_DHT.read_retry(11,4)
+    elapsed_measure = time.time() - start_measure
+    measurements.append(t)
     print('Temp: {0:0.3f} C  Hum: {1:0.3f} %'.format(t[1], t[0]))
-    if len(measurments) == MEASURMENTS_PER_INTERVAL:
-        avg_hum = sum([t[0] for t in measurments])/MEASURMENTS_PER_INTERVAL
-        avg_temp = sum([t[1] for t in measurments])/MEASURMENTS_PER_INTERVAL
+    if len(measurements) == MEASUREMENTS_PER_INTERVAL:
+        avg_hum = sum([t[0] for t in measurements])/MEASUREMENTS_PER_INTERVAL
+        avg_temp = sum([t[1] for t in measurements])/MEASUREMENTS_PER_INTERVAL
         print('Temp: {0:0.3f} C  Hum: {1:0.3f} %'.format(avg_temp, avg_hum))
         # TODO:  Insert into DB
         # Reset queue
-        measurments = []
-
-    time.sleep(MEASURMENT_TIME)
+        elapsed = time.time() - start
+        print("Total elapsed time: " + str(elapsed))
+        start = time.time()
+        measurements = []
+    if MEASUREMENT_TIME - elapsed_measure > 0:
+        time.sleep(MEASUREMENT_TIME - elapsed_measure)
