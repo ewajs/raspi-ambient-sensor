775f108ee0519f4d0143332febe6d4f6cedf3e1b
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2019-11-03 10:35:59 -0300

    Reorganized folders. Sensor running on Docker

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This is a simple script that creates a database and stores time, temperature and humidity using an I2C Sensor hooked to a Raspberry Pi.
 
 Also included, is a flask webserver that will serve the data over HTTP and plot it in a web Browser window.
+
+## Connections
+- `+` Pin in the sensor hooks up with RPI3 Pin 4 (5 VDC).
+- `-` Pin in the sensor hooks up with RPI3 Pin 6 (GND).
+- `out` Pin in the sensor hooks up with RPI3 Pin 7 (GPIO04).
+
+## Running the containers
+### With Docker
+The sensor container needs acces to the GPIO ports, in order to grant those the container has to be initialized with privileges and the `/dev/mem` volume of the host mounted to the container in the same path:
+```
+docker container run -it --privileged --device=/dev/mem:/dev/mem ewajs/rpi3sensor
+```
--- sensor/sensor.py
+++ sensor/sensor.py
@@ -7,10 +7,10 @@ import time
 conn = sqlite3.connect('temphum.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS Ambient 
-	(id INTEGER PRIMARY KEY AUTOINCREMENT,
-	Temperature REAL NOT NULL,
-	Humidity REAL NOT NULL,
-	Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Temperature REAL NOT NULL,
+    Humidity REAL NOT NULL,
+    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
 conn.commit()
 DATA_TIME = 300 # In seconds
 MEASUREMENTS_PER_INTERVAL = 50 
@@ -19,7 +19,7 @@ MEASUREMENT_TIME = DATA_TIME // MEASUREMENTS_PER_INTERVAL
 measurements = []
 start = time.time()    
 while True:
-	start_measure = time.time()
+    start_measure = time.time()
     t = Adafruit_DHT.read_retry(11,4)
     elapsed_measure = time.time() - start_measure
     measurements.append(t)
@@ -29,10 +29,10 @@ while True:
         avg_temp = sum([t[1] for t in measurements])/MEASUREMENTS_PER_INTERVAL
         print('Temp: {0:0.3f} C  Hum: {1:0.3f} %'.format(avg_temp, avg_hum))
         # Insert into DB
-		c = conn.cursor()
-		c.execute('''INSERT INTO Ambient (Humidity,Temperature) VALUES
-		({0:0.3f},{1:0.3f})'''.format(avg_hum,avg_temp))
-		conn.commit()
+        c = conn.cursor()
+        c.execute('''INSERT INTO Ambient (Humidity,Temperature) VALUES
+        ({0:0.3f},{1:0.3f})'''.format(avg_hum,avg_temp))
+        conn.commit()
         # Reset queue
         elapsed = time.time() - start
         print("Total elapsed time: " + str(elapsed))
